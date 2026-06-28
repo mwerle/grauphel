76b0326a8225ce14c51d7d704a42f68189754f41
Michael Werle <micha@michaelwerle.com>
2026-06-28 14:23:21 +0900

    Clean up nav bar display

--- templates/appnavigation.php
+++ templates/appnavigation.php
@@ -1,19 +1,32 @@
 <?php script('grauphel', 'grauphel'); ?>
 
 <div id="app-navigation">
-  <ul>
+  <!-- Tags list -->
+   <div class="app-navigation-header">Tags</div>
+  <ul class="app-navigation-list">
     <?php foreach ($_['tags'] as $tag) { ?>
-      <li data-id="<?php p($tag['id']) ?>" <?php $tag['selected'] && print ' class="selected"'; ?>><a href="<?php p($tag['href']) ?>"><?php p($tag['name']); ?></a></li>
+      <li class="app-navigation-entry <?php $tag['selected'] && print 'selected' ?>"
+          data-id="<?php p($tag['id']) ?>">
+        <a href="<?php p($tag['href']) ?>"><?php p($tag['name']); ?></a>
+      </li>
     <?php } ?>
   </ul>
 
+  <!-- Statistics -->
+  <div class="app-navigation-info">
+    <ul>
+      <li>Notes: <?php p($_['notes_count']); ?></li>
+      <li>Tags: <?php p($_['tags_count']); ?></li>
+  </ul>
+  </div>
+
+  <!-- Settings button -->
   <div id="app-settings">
-	<ul>
-	<li><div id="app-settings-header">Notes: <?php p($_['notes_count']); ?></div></li>
-	<li><div id="app-settings-header">Tags: <?php p($_['tags_count']); ?></div></li>
-    <li><div id="app-settings-header">
-      <a href="<?php p($_['urlGen']->linkToRoute('grauphel.gui.settings')); ?>" class="settings-button" aria-label="Grauphel settings">Grauphel settings</a>
-    </div></li>
-</ul>
+    <ul>
+      <li><div id="app-settings-header">
+        <a href="<?php p($_['urlGen']->linkToRoute('grauphel.gui.settings')); ?>"
+           class="settings-button" aria-label="Grauphel settings">Grauphel settings</a>
+      </div></li>
+    </ul>
   </div>
 </div>
